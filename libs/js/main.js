0f7eba172e47060f53fe2297feaaec2243a72771
Luis Maria Gomez <chuni12gomez@gmail.com>
2023-03-18 16:39:35 -0300

    final

---
+++ libs/js/main.js
@@ -0,0 +1,28 @@
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
+
+const btn_scrolltop = document.getElementById("btn_scrolltop")
+btn_scrolltop.addEventListener('click', () => {
+  window.scrollTo(0, 0)
+})
+
+window.onscroll = () => {
+add_btn_scrolltop()
+}
+
+const add_btn_scrolltop = () => {
+if (window.scrollY < 300) {
+  btn_scrolltop.classList.remove("btn-scrolltop-on")
+} else {
+  btn_scrolltop.classList.add("btn-scrolltop-on")
+}
+}
